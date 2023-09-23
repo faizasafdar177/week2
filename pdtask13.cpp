d2c94dec8a0af2be83b2b9c94d626682ac420ce9
faiza kiran <144409199+faizasafdar177@users.noreply.github.com>
2023-09-23 05:09:00 -0700

    Add files via upload

---
+++ pdtask13.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	system("color 01");	
+	cout << "   +--^---------- , -------- , ----- , --------^-,    "<<endl;
+	cout << "    | |||||||||     '--------'        |            o  "<<endl;
+	cout << "    '+--------------------------------^------------|  "<<endl;
+	cout << "      ',----------,------------,------------------'   "<<endl;
+	cout << "        / XXXXXX  /'|          /'                     "<<endl;
+	cout << "       / XXXXXX  /  |         /'                      "<<endl;
+	cout << "      / XXXXXX  /'----------,                         "<<endl;
+	cout << "     / XXXXXX  /                                      "<<endl;
+	cout << "    / XXXXXX  /                                       "<<endl;
+	cout << "   (----------(                                       "<<endl;
+	cout << "    '--------'                                        "<<endl;
+}
